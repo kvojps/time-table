9ebf9b7607e8c2020c6285bcf615b84606eeb834
Jose Ferreira <josebazjunior09@gmail.com>
2023-09-07 15:41:45 -0300

    [feat]: adding generate lesson list

--- utils/example_dto.py
+++ utils/example_dto.py
@@ -79,6 +79,129 @@ example_dto = {
             "hour_end_time": 15,
             "minute_end_time": 30
         }
+    ],
+    "lesson_list": [
+        {
+            "id": 1,
+            "subject": "Math",
+            "teacher": "A. Turing",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 2,
+            "subject": "Math",
+            "teacher": "A. Turing",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 3,
+            "subject": "Physics",
+            "teacher": "M. Curie",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 4,
+            "subject": "Chemistry",
+            "teacher": "M. Curie",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 5,
+            "subject": "Biology",
+            "teacher": "C. Darwin",
+            "student_group": "9th grade"
+
+        },
+        {
+            "id": 6,
+            "subject": "History",
+            "teacher": "I. Jones",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 7,
+            "subject": "English",
+            "teacher": "I. Jones",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 8,
+            "subject": "English",
+            "teacher": "I. Jones",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 9,
+            "subject": "Spanish",
+            "teacher": "P. Cruz",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 10,
+            "subject": "Spanish",
+            "teacher": "P. Cruz",
+            "student_group": "9th grade"
+        },
+        {
+            "id": 11,
+            "subject": "Math",
+            "teacher": "A. Turing",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 12,
+            "subject": "Math",
+            "teacher": "A. Turing",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 13,
+            "subject": "Math",
+            "teacher": "A. Turing",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 14,
+            "subject": "Physics",
+            "teacher": "M. Curie",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 15,
+            "subject": "Chemistry",
+            "teacher": "M. Curie",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 16,
+            "subject": "French",
+            "teacher": "M. Curie",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 17,
+            "subject": "Geography",
+            "teacher": "C. Darwin",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 18,
+            "subject": "History",
+            "teacher": "I. Jones",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 19,
+            "subject": "English",
+            "teacher": "P. Cruz",
+            "student_group": "10th grade"
+        },
+        {
+            "id": 20,
+            "subject": "Spanish",
+            "teacher": "P. Cruz",
+            "student_group": "10th grade"
+        }
     ]
 
 }
--- utils/generate_problem.py
+++ utils/generate_problem.py
@@ -4,28 +4,7 @@ from datetime import time
 
 def generate_problem(time_scheduler_dto):
     timeslot_list = generate_time_slot_list(time_scheduler_dto)
-    lesson_list = [
-        Lesson(1, "Math", "A. Turing", "9th grade"),
-        Lesson(2, "Math", "A. Turing", "9th grade"),
-        Lesson(3, "Physics", "M. Curie", "9th grade"),
-        Lesson(4, "Chemistry", "M. Curie", "9th grade"),
-        Lesson(5, "Biology", "C. Darwin", "9th grade"),
-        Lesson(6, "History", "I. Jones", "9th grade"),
-        Lesson(7, "English", "I. Jones", "9th grade"),
-        Lesson(8, "English", "I. Jones", "9th grade"),
-        Lesson(9, "Spanish", "P. Cruz", "9th grade"),
-        Lesson(10, "Spanish", "P. Cruz", "9th grade"),
-        Lesson(11, "Math", "A. Turing", "10th grade"),
-        Lesson(12, "Math", "A. Turing", "10th grade"),
-        Lesson(13, "Math", "A. Turing", "10th grade"),
-        Lesson(14, "Physics", "M. Curie", "10th grade"),
-        Lesson(15, "Chemistry", "M. Curie", "10th grade"),
-        Lesson(16, "French", "M. Curie", "10th grade"),
-        Lesson(17, "Geography", "C. Darwin", "10th grade"),
-        Lesson(18, "History", "I. Jones", "10th grade"),
-        Lesson(19, "English", "P. Cruz", "10th grade"),
-        Lesson(20, "Spanish", "P. Cruz", "10th grade"),
-    ]
+    lesson_list = generate_lesson_list(time_scheduler_dto)
     lesson = lesson_list[0]
     lesson.set_timeslot(timeslot_list[0])
 
@@ -41,3 +20,12 @@ def generate_time_slot_list(time_scheduler_dto):
         timeslot_list.append(time_slot_add)
 
     return timeslot_list
+
+
+def generate_lesson_list(time_scheduler_dto):
+    lesson_list = []
+    for lesson in time_scheduler_dto["lesson_list"]:
+        lesson_add = Lesson(lesson["id"], lesson["subject"], lesson["teacher"], lesson["student_group"])
+        lesson_list.append(lesson_add)
+
+    return lesson_list
